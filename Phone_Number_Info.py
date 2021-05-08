f2eef69e0558ef6e12732bd49d73e56fe4074aba
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-08 11:40:47 +0530

    Update Phone_Number_Info.py

--- Phone_Number_Info.py
+++ Phone_Number_Info.py
@@ -11,26 +11,26 @@ while True:
             b = phonenumbers.parse("+91"+a)
             x = carrier.name_for_number(b, 'en')
             if geocoder.description_for_number(b, 'en') == "" or carrier.name_for_number(b, 'en') == "":
-                notification.notify(title="Phone Number Info by : Satyam Tripathi",
+                notification.notify(title="Phone Number Info by: Satyam Tripathi",
                                     message="\n\tAlert! Number is Invalid, Please Try Again",
                                     app_icon="C:/Users"
                                              "/Dell/Downloads/err.ico", timeout=7)
             else:
-                notification.notify(title="Phone Number Info by : Satyam Tripathi",
+                notification.notify(title="Phone Number Info by: Satyam Tripathi",
                                     message=f"\n\t  Your Phone Number : {a[:]}"
-                                    f"\n\t  Your Country : {geocoder.description_for_number(b, 'en').upper()}\n\t  SIM"
-                                    f" Card : {carrier.name_for_number(b, 'en').title()}",
+                                    f"\n\t  Your Country: {geocoder.description_for_number(b, 'en').upper()}\n\t  SIM"
+                                    f" Card: {carrier.name_for_number(b, 'en').title()}",
                                     app_icon="C:/Users"
                                              "/Dell/Downloads/sim.ico", timeout=7)
 
         except:
-            notification.notify(title="Phone Number Info by : Satyam Tripathi",
+            notification.notify(title="Phone Number Info by: Satyam Tripathi",
                                 message="\n\tAlert! Number is Invalid, Please Try Again",
                                 app_icon="C:/Users"
                                          "/Dell/Downloads/err.ico", timeout=7)
 
     else:
-        notification.notify(title="Phone Number Info by : Satyam Tripathi",
+        notification.notify(title="Phone Number Info by: Satyam Tripathi",
                             message="\n\tAlert! Number is Invalid, Please Try Again",
                             app_icon="C:/Users"
                                      "/Dell/Downloads/err.ico", timeout=7)
